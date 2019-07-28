f3d028f8a91588f7f4822a3d9e4119bc035ab1f8
RyBo <ryanccrawford@live.com>
2019-07-28 01:37:37 -0400

    Resized all gifs so they would load faster

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -10,7 +10,7 @@ class Board extends Component {
     }
     constructor(props) {
         super(props);
-        this.state.images = ["13", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "14", "15", "16"] ;
+        this.state.images = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"] ;
         this.state.cardsClick = [];
         this.state.cards = [];
         if (props.shuffleNow) {
