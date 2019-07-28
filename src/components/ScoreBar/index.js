389a9e27b3d8c92e8d86f432f918da01a8317beb
RyBo <ryanccrawford@live.com>
2019-07-28 04:27:31 -0400

    Made Score larger

--- src/components/ScoreBar/index.js
+++ src/components/ScoreBar/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-
+import "./style.css";
 
 class ScoreBar extends Component {
   
     render() {
         return (
             <div>
-                <span className="badge-pill badge badge-secondary ml-1">Current Score: {`${this.props.score}`}</span>
-                <span className="badge-pill badge badge-secondary ml-1">Wins: {`${this.props.wins}`}</span>
-                <span className="badge-pill badge badge-secondary ml-1">Losses: {`${this.props.loses}`}</span>
-                <span className="badge-pill badge badge-secondary ml-1">High Score: {`${this.props.highScore}`}</span>
+                <span className="badge-pill badge badge-secondary ml-1 large">Current Score: {`${this.props.score}`}</span>
+                <span className="badge-pill badge badge-secondary ml-1 large">Wins: {`${this.props.wins}`}</span>
+                <span className="badge-pill badge badge-secondary ml-1 large">Losses: {`${this.props.loses}`}</span>
+                <span className="badge-pill badge badge-secondary ml-1 large">High Score: {`${this.props.highScore}`}</span>
 
             </div>
 
